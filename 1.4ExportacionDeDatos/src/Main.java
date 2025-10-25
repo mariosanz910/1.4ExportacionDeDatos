5d2467bfa568cbc26e61c6e10b6b8e8c4aef4a7e
mariosanz910 <mario.sanzvillarejo24@campusfp.es>
2025-10-25 13:09:49 +0200

    Add HotelMain execution to main method

--- 1.4ExportacionDeDatos/src/Main.java
+++ 1.4ExportacionDeDatos/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         new EstudiantesMain().main(args);
         new ProductosMain().main(args);
+        new HotelMain().main(args);
     }
 }
